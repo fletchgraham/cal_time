dc5ff90c9bd9fdb0692fbb0806592d2ea04ccc99
fletchgraham <sfletchgraham@gmail.com>
2019-12-15 18:29:53 -0500

    prints duration of projects

--- time.py
+++ time.py
@@ -6,6 +6,10 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+import sys
+
+import dateparser
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
@@ -36,17 +40,36 @@ def main():
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
+    print(now)
+    '''events_result = service.events().list(calendarId='primary', timeMin=now,
                                         maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()'''
+    
+    events_result = service.events().list(calendarId='primary', singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
+        print('No events.')
+
+    projects = {}
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        title = str(event['summary'])
+        if not title in projects.keys():
+            projects[title] = duration(event)
+        else:
+            projects[title] += duration(event)
+            
+    for project, dur in projects.items():
+        print(project, dur.total_seconds() / (60 * 60), 'hours')
+
+def duration(event):
+    start = dateparser.parse(str(event['start'].get('dateTime')))
+    end = dateparser.parse(str(event['end'].get('dateTime')))
+    if not start or not end:
+        return datetime.timedelta(0)
+    return end - start
+    
 
 if __name__ == '__main__':
     main()
